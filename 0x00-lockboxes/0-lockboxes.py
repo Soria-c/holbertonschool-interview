fb28f7fffe8f4e22f6a1f878234b473b10aa604b
Carlos Soria <soriac367@gmail.com>
2022-12-08 09:41:15 -0500

    Method that determines if all the boxes can be opened.

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Lockboxes"""
+
+
+def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened."""
+    visited = {}
+    len_v = len(visited)
+    rem_boxes = len(boxes) - 1
+    keys = set(boxes[0])
+    total_keys = keys
+
+    while(len_v != rem_boxes):
+        k = set()
+        for i in keys:
+            if (i):
+                k = k.union(set(boxes[i]))
+                index = str(i)
+                if (not visited.get(index)):
+                    visited[index] = True
+        if (len(visited) == len_v):
+            break
+        len_v = len(visited)
+        keys = k.difference(total_keys)
+        total_keys = total_keys.union(keys)
+    return len(total_keys - {0}) == rem_boxes
