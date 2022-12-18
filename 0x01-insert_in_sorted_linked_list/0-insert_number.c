4d54a148737a3fac49012524f2a66a5e36ee561a
Carlos Soria <soriac367@gmail.com>
2022-12-18 09:08:17 -0500

    Insert in sorted linked list

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -11,7 +11,7 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *node;
 	listint_t *tmp;
 
-	if (!head || !(*head))
+	if (!head)
 		return (NULL);
 	node = malloc(sizeof(listint_t));
 	if (!node)
