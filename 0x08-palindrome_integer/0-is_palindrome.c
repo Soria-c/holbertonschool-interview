0105a244884d1e03770d8195ce62d94f2edd9b6c
Carlos Soria <soriac367@gmail.com>
2023-02-08 11:30:08 -0500

    Refactor

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -6,11 +6,10 @@
 */
 int is_palindrome(unsigned long n)
 {
-	unsigned long n2 = n;
 	unsigned long l_digit = 1, r_digit = 10;
 
-	for (; n2; l_digit *= 10)
-		n2 /= 10;
+	for (; (unsigned long)(n / l_digit); l_digit *= 10)
+		;
 	for (l_digit /= 10;
 		(l_digit >= r_digit) &&
 		(((n / l_digit) % 10) == ((n % r_digit) / (r_digit / 10)));
