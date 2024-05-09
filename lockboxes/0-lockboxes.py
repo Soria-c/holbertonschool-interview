da5cd43fd65d43b7ca23cdc472c6959cc18759bd
Carlos Soria <soriac367@gmail.com>
2024-05-09 10:07:54 -0500

    lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -5,15 +5,15 @@
 def canUnlockAll(boxes):
     """Method that determines if all the boxes can be opened."""
     box_len = len(boxes)
-    keys = set({0, *boxes[0]})
+    keys = set(boxes[0])
     i = 1
-    while i < box_len - 1 and len(keys) != box_len:
+    while i < box_len - 1 and len(keys) != box_len - 1:
         if i in keys:
             for j in boxes[i]:
                 if j < box_len:
                     keys.add(j)
                     keys = keys.union(filter(lambda x: x < box_len, boxes[j]))
-                if (len(keys) == box_len):
+                if (len(keys) == box_len - 1):
                     break
         i += 1
-    return len(keys) == box_len
+    return len(keys) == box_len - 1
