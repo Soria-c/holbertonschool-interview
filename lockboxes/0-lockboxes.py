03fb34a0d3bf4ecc63241454c4b346f0f0e8c11e
Carlos Soria <soriac367@gmail.com>
2024-05-09 09:56:47 -0500

    lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
-"""This module contains the function canUnlockAll
-    which is a method that determines if all the boxes
-    can be opened.
-"""
+"""Lockboxes"""
 
 
 def canUnlockAll(boxes):
-    """
-    Method that determines if all the boxes can be opened.
-    ```
-    Parameters:
-    -----------
-    boxes: list[][]
-        boxes is a list of lists
-    """
-    box_len = len(boxes)
-    keys = set({0, *boxes[0]})
-    i = 1
-    while i < box_len - 1 and len(keys) != box_len:
-        if i in keys:
-            for j in boxes[i]:
-                if j < box_len:
-                    keys.add(j)
-                    keys = keys.union(filter(lambda x: x < box_len, boxes[j]))
-                if (len(keys) == box_len):
-                    break
-        i += 1
-    return len(keys) == box_len
+    """Method that determines if all the boxes can be opened."""
+
+    visited = {}
+    len_v = len(visited)
+    rem_boxes = len(boxes) - 1
+    keys = set(boxes[0])
+    total_keys = keys
+
+    while(len_v != rem_boxes):
+        k = set()
+        for i in keys:
+            if (i and i < (rem_boxes + 1)):
+                k = k.union(set(boxes[i]))
+                visited[str(i)] = True
+        if (len(visited) == len_v):
+            break
+        len_v = len(visited)
+        keys = k.difference(total_keys) - {0}
+        total_keys = total_keys.union(keys)
+
+    return len_v == rem_boxes
