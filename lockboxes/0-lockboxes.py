e164e8d2385b6b9223d8d13965ee1c42162e9df6
Carlos Soria <soriac367@gmail.com>
2024-05-09 09:12:33 -0500

    lockboxes

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Lockboxes"""
+
+
+def canUnlockAll(boxes):
+    """
+    Method that determines if all the boxes can be opened.
+    ```
+    Parameters:
+    -----------
+    boxes: list[][]
+        boxes is a list of lists
+    """
+    box_len = len(boxes)
+    keys = set({0, *boxes[0]})
+    i = 1
+    while i < box_len - 1 and len(keys) != box_len:
+        if i in keys:
+            for j in boxes[i]:
+                if j < box_len:
+                    keys.add(j)
+                    keys = keys.union(filter(lambda x: x < box_len, boxes[j]))
+                if (len(keys) == box_len):
+                    break
+        i += 1
+    return len(keys) == box_len
